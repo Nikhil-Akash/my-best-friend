68e0c9355cb52d619b9f8623c4ed1c01889c8d3e
github username <github email address>
2018-06-03 00:35:13 -0700

    added google font Catamaran and Imprima to body and h1-h6

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../Routes';
-// import './App.css';
+import './App.css';
 
 class App extends Component {
   render() {
