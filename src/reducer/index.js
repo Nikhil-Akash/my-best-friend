e7f0c6854d9ad37cb8d73459550de161bfef4881
github username <github email address>
2018-06-04 22:42:21 -0700

    added breedReducer file to index as combinedReducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import breedsReducer from './breedsReducer';
+import breedReducer from './breedReducer';
 
 const rootReducer = combineReducers({
  breeds: breedsReducer,
+ breed: breedReducer,
 });
 
 export default rootReducer;
