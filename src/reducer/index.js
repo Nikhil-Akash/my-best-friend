09efce2e13022fb8d9d979b8a3058a610e21ac17
github username <github email address>
2018-06-05 04:02:09 -0700

    added subBreedReducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import breedsReducer from './breedsReducer';
 import breedReducer from './breedReducer';
+import subBreedReducer from './subBreedReducer';
 
 const rootReducer = combineReducers({
  breeds: breedsReducer,
  breed: breedReducer,
+ subBreed: subBreedReducer,
 });
 
 export default rootReducer;
