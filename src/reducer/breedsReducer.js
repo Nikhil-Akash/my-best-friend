5b5f213394cda1a201d11813a38352f3863a91fa
github username <github email address>
2018-06-04 22:42:49 -0700

    separated reducers into their own file

--- src/reducer/breedsReducer.js
+++ src/reducer/breedsReducer.js
@@ -3,13 +3,7 @@ import * as types from '../action/actionTypes';
 export default (state = [], action) => {
   switch (action.type) {
     case types.REQUEST_ALL_BREEDS:
-      return action.breeds;
-
-    case types.REQUEST_BREED_FAMILY:
-      return action.breed;
-
-    // case types.REQUEST_SUB_BREED:
-    //   return action.breed;
+      return action.breeds
 
     default:
       return state;
