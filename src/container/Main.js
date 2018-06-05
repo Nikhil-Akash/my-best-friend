96dc475be4da83e6d43b56f18686f8acc38e3902
github username <github email address>
2018-06-05 03:27:57 -0700

    refactored code; Changed table object into a nav menu in BreedMenu container; moved SelectedBreed component to BreedMenu

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { loadAllBreeds, selectBreed, selectSubBreed } from '../action/index';
+import { loadAllBreeds } from '../action/index';
+import BreedMenu from './breedMenu'
 import SelectedBreed from './selectedBreed'
 
 class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      allBreeds: false,
+      chooseBreed: false,
       selectedBreed: false,
-      breedName: ""
     }
   }
 
-  toggleSelectedBreed = (breedName) => {
+  toggleChooseBreed = () => {
     this.setState({
-      selectedBreed: !this.state.selectBreed,
-      breedName: breedName,
+      chooseBreed: !this.state.chooseBreed,
     })
-    this.props.selectBreed(breedName)
   }
 
   componentDidMount() {
@@ -26,54 +24,21 @@ class Main extends Component {
   }
 
   render() {
-    const listOfBreeds = Object.keys(this.props.breeds);
-
-    const breedNames = listOfBreeds.map((breedName, index) =>
-      <tr key={index} className="gds-table__row">
-        <td key={index}><a onClick={() => this.toggleSelectedBreed(breedName)}>{breedName}</a></td>
-      </tr>
-    );
-
     return (
-
       <main className="gds-segment">
+        <button type="button" onClick={() => this.toggleChooseBreed()}>Choose A Breed</button>
 
-        {/* The Options - Search Bar & Choose Breed Button  */}
-        {/* <button type="button" onClick={this.toggleAllBreeds}>Choose Your Breed</button> */}
-
-        {/* Default view - a table of all breeds grouped by First Letter */}
-        {/* when breed is selected, state.allBreeds is false, replace all the page content in main with Selected Breed container */}
-        {/* Selected Breed view will have a side nav bar to select another breed; when selected, the side nav bar will clear off side */}
-
-        {(!this.state.allBreeds && !this.state.selectedBreed) ?
-          <div>
-            <div>
-              <input placeholder="Search Breed" />
-              <h2 className="-text-center">Choose Your Breed</h2>
-            </div>
-            <div className="gds-table--responsive">
-              <table className="gds-table gds-table--sm" data-gds-table="">
-                <thead>
-                  <tr className="gds-table__row">
-                    <th className="gds-table__header" data-gds-table-header="">ALL BREEDS</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="gds-table__row">
-                    <td># of Breeds: {listOfBreeds.length}</td>
-                  </tr>
-                  {breedNames}
-                </tbody>
-              </table>
-            </div>
+        {!this.state.chooseBreed ?
+          <div className="gds-segment">
+            <h1 className="-text-center">All Dogs</h1>
+            <p className="-text-center">Gallery of dogs</p>
           </div>
         :
-          <SelectedBreed
-            breed={this.props.breed}
-            breedName={this.state.breedName}
+          <BreedMenu
+            breeds={this.props.breeds}
+            selectedBreed={this.state.selectedBreed}
           />
         }
-
       </main>
     );
   }
@@ -86,4 +51,4 @@ const mapStatesToProps = (state) => {
   });
 };
 
-export default connect(mapStatesToProps, { loadAllBreeds, selectBreed, selectSubBreed })(Main);
+export default connect(mapStatesToProps, { loadAllBreeds })(Main);
