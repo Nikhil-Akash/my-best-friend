2ebf7284b6ed7c3c92606e65dd7b79a32fd53ec4
github username <github email address>
2018-06-06 22:10:51 -0700

    added alt to top 5 image

--- src/container/subBreedMenu.js
+++ src/container/subBreedMenu.js
@@ -19,11 +19,10 @@ class SubBreedMenu extends Component {
   }
 
   render() {
-    // const theSubBreeds = this.props.subBreedList.map((subBreed, index) => <td key={index} onClick={()=>this.toggleSelectSubBreed(index)}>{subBreed}</td>)
 
     const theSubBreeds = this.props.subBreedList.map((subBreed, index) =>     <li key={index} class="gds-ranker__item">
             <div class="gds-ranker__item-icon" data-tooltip={subBreed}>
-                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" alt={subBreed}/>
             </div>
             <div class="gds-ranker__item-number gds-ranker__item-number--secondary">{subBreed}</div>
         </li>)
