c79a4c9747336e116905e16df93156fa5445f2f9
github username <github email address>
2018-06-06 19:55:45 -0700

    updated all images in a thumbnail grid in the leaderboard style

--- src/container/selectedBreed.js
+++ src/container/selectedBreed.js
@@ -5,11 +5,18 @@ class SelectedBreed extends Component {
 
   render() {
 
-    const allImages = this.props.breed.map((image, index) => <img key={index} class="gds-image" src={image} alt="Dog"/> );
+    const allImages = this.props.breed.map((image, index) => <div>
+        <img key={index} className="gds-ranker-ensign__thumbnail gds-ranker-ensign__thumbnail--lg" src={image} alt="Dog"/>
+
+          <span className="gds-ranker-ensign__number">
+            {index}
+          </span>
+      </div>);
+
 
     return (
       <div className="selectedBreed">
-        <h1 className="gds-text--header-md -text-center">{this.props.breedName}</h1>
+        <h1 className="gds-text--header-md  -text-center">{this.props.breedName}</h1>
 
         {(this.props.subBreedList.length > 0) ?
           <SubBreedMenu
@@ -25,9 +32,49 @@ class SelectedBreed extends Component {
           <p>hello</p>
 
         :
-          <div className="gds-container">
-            {allImages}
-          </div>
+        <div class="gds-leaderboard">
+            <div class="gds-leaderboard__topfive">
+                <div class="gds-ranker gds-ranker--lg">
+                    <ul class="gds-ranker__list">
+                        <li class="gds-ranker__item">
+                            <div class="gds-ranker__item-icon" data-tooltip="Brand Name goes here">
+                                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                            </div>
+                            <div class="gds-ranker__item-number gds-ranker__item-number--secondary">1<span class="gds-ranker__item-number-ordinal">th</span></div>
+                        </li>
+                        <li class="gds-ranker__item">
+                            <div class="gds-ranker__item-icon" data-tooltip="Brand Name goes here">
+                                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                            </div>
+                            <div class="gds-ranker__item-number gds-ranker__item-number--secondary">2<span class="gds-ranker__item-number-ordinal">th</span></div>
+                        </li>
+                        <li class="gds-ranker__item">
+                            <div class="gds-ranker__item-icon" data-tooltip="Brand Name goes here">
+                                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                            </div>
+                            <div class="gds-ranker__item-number gds-ranker__item-number--secondary">3<span class="gds-ranker__item-number-ordinal">th</span></div>
+                        </li>
+                        <li class="gds-ranker__item">
+                            <div class="gds-ranker__item-icon" data-tooltip="Brand Name goes here">
+                                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                            </div>
+                            <div class="gds-ranker__item-number gds-ranker__item-number--secondary">4<span class="gds-ranker__item-number-ordinal">th</span></div>
+                        </li>
+                        <li class="gds-ranker__item">
+                            <div class="gds-ranker__item-icon" data-tooltip="Brand Name goes here">
+                                <img class="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" />
+                            </div>
+                            <div class="gds-ranker__item-number gds-ranker__item-number--secondary">5<span class="gds-ranker__item-number-ordinal">th</span></div>
+                        </li>
+                    </ul>
+                    <div class="gds-ranker__connector"></div>
+                </div>
+            </div>
+            <div class="gds-leaderboard__grid">
+              {allImages}
+            </div>
+        </div>
+
         }
 
       </div>
