9e76e723a22cd69721c5aad0056a40eb6465c82d
github username <github email address>
2018-06-10 20:21:20 -0700

    added LogoButton component to Header; added a link to BreedMenu component with link of All Dogs;

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import LogoButton from './LogoButton';
+import AllBreeds from './AllBreeds';
+import BreedMenu from '../container/breedMenu';
 
-const Header = () => {
-  return (
-    <header className="-color-bg-pri -text-center">
-      <h1 className="gds-text--header-lg">My Best Friend</h1>
-    </header>
-  );
+class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      chooseBreed: false,
+    }
+  }
+
+  toggleChooseBreed = () => {
+    this.setState({
+      chooseBreed: !this.state.chooseBreed,
+    })
+  }
+  render() {
+    return (
+      <header>
+        <LogoButton />
+        <AllBreeds toggleChooseBreed={this.toggleChooseBreed} />
+
+        {!this.state.chooseBreed ?
+          null
+        :
+          <BreedMenu />
+        }
+
+      </header>
+    );
+  }
+
 }
 
 export default Header;
+
+// <header className="-color-bg-pri -text-center">
+//   <h1 className="gds-text--header-lg">My Best Friend</h1>
+// </header>
