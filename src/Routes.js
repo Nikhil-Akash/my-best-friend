1a9e4c680203d235ab88d03d20ab9fea260b9864
github username <github email address>
2018-06-10 20:22:48 -0700

    ,

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import WelcomePage from './component/WelcomePage';
 import Main from './container/Main'
+import selectedBreed from './container/selectedBreed'
 
 class Router extends Component {
   render() {
@@ -10,7 +12,9 @@ class Router extends Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={WelcomePage} />
+          <Route exact path="/all_dogs" component={Main} />
+          <Route exact path="/all_dogs/selected_breed" component={selectedBreed} />
         </Switch>
         <Footer />
       </div>
