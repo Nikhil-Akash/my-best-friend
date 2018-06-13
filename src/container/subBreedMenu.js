f261780767bb585f3b208697bbf25801b6830eef
github username <github email address>
2018-06-12 18:33:59 -0700

    moved subBreedImg code to ImgGrid Component

--- src/container/subBreedMenu.js
+++ src/container/subBreedMenu.js
@@ -18,32 +18,23 @@ class SubBreedMenu extends Component {
   }
 
   render() {
-    debugger
     const breedName = this.props.breedName
-    const theSubBreeds = this.props.haveSubBreeds.map((subBreed, index) =>     <li key={index} className="gds-ranker__item">
-            <div className="gds-ranker__item-icon" data-tooltip={subBreed}>
-                <img className="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" alt={subBreed} onClick={()=>this.toggleSelectSubBreed(breedName, subBreed)}/>
-            </div>
-            <div className="gds-ranker__item-number gds-ranker__item-number--secondary">{subBreed}</div>
-        </li>)
+    const theSubBreeds = this.props.haveSubBreeds.map((subBreed, index) =>
+      <li key={index} className="gds-ranker__item">
+        <div className="gds-ranker__item-icon" data-tooltip={subBreed}>
+          <img className="gds-ranker__image" src="https://assets.ggops.com/images/ken.png" alt={subBreed} onClick={()=>this.toggleSelectSubBreed(breedName, subBreed)}/>
+        </div>
+        <div className="gds-ranker__item-number gds-ranker__item-number--secondary">{index}</div>
+      </li>
+    );
 
-    const subBreedImg = this.props.subBreed.map((subBreedImg, index) => <div key={index}>
-        <img key={index} className="gds-ranker-ensign__thumbnail gds-ranker-ensign__thumbnail--lg" src={subBreedImg} alt="Dog"/>
-
-          <span className="gds-ranker-ensign__number">
-            {index}
-          </span>
-      </div>);
 
     return (
-      <div className="gds-leaderboard__topfive">
-          <div className="gds-ranker gds-ranker--lg">
-            <ul className="gds-ranker__list">
-                {theSubBreeds}
-            </ul>
-            <div className="gds-ranker__connector"></div>
-          </div>
-          {subBreedImg}
+      <div className="gds-ranker gds-ranker--lg">
+        <ul className="gds-ranker__list">
+          {theSubBreeds}
+        </ul>
+        <div className="gds-ranker__connector"></div>
       </div>
     );
   }
