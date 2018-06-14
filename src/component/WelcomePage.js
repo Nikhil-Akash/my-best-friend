77fe46d8e548ae00ee90d9348bb365258b283139
github username <github email address>
2018-06-13 20:16:20 -0700

    centered content

--- src/component/WelcomePage.js
+++ src/component/WelcomePage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
   return (
-    <div>
+    <div className="-text-center">
       <h1 className="gds-text--header-lg">Welcome to My Best Friend</h1>
       <p className=" -text-center ">put picture of a dog here</p>
       <Link to={`/all_dogs`}><button type="button" id="allDogsButton" className="gds-button--default">All Dogs</button></Link>
