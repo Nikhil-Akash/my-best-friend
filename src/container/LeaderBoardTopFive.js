0497be831481944f4f3b6af1563f9380b5155884
github username <github email address>
2018-06-13 19:24:49 -0700

    added conditional statement to populate images of selected breed through MainBreed or SubBreed ImgList component

--- src/container/LeaderBoardTopFive.js
+++ src/container/LeaderBoardTopFive.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import SubBreedMenu from './subBreedMenu';
+import MainBreedImgList from './MainBreedImgList';
+import SubBreedImgList from './SubBreedImgList';
 
 class TopFive extends Component {
+
   render() {
     return (
-      <div className="gds-leaderboard__topfive">
-        {(this.props.haveSubBreeds <= 0) ?
-            <p>One of a kind. There are no sub-breeds for this dog.</p>
+      <div>
+        <div>
+          {(this.props.haveSubBreeds <= 0) ?
+              <h4 className="-text-center">One of a kind. There are no sub-breeds for this dog.</h4>
+            :
+              <SubBreedMenu
+                breedName={this.props.breedName}
+                haveSubBreeds={this.props.haveSubBreeds}
+                subBreed={this.props.subBreed}
+                selectedSubBreed={this.props.selectedSubBreed}
+              />
+          }
+        </div>
+        <div>
+          {(this.props.haveSubBreeds <= 0) ?
+            <MainBreedImgList
+              breed={this.props.breed}
+            />
           :
-            <SubBreedMenu
-              breedName={this.props.breedName}
-              haveSubBreeds={this.props.haveSubBreeds}
+            <SubBreedImgList  subBreed={this.props.subBreed}
             />
           }
+        </div>
       </div>
     );
   }
 }
 
-export default TopFive;
+export default TopFive
