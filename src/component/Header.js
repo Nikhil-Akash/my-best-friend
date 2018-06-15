ccb16ec94334fb6e4d9e493ee2472720f98baacb
github username <github email address>
2018-06-14 22:27:16 -0700

    changed class to className

--- src/component/Header.js
+++ src/component/Header.js
@@ -17,11 +17,11 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <nav class="-m-b-3" data-gds-nav-controls="">
-          <ul class="gds-nav-tabs gds-nav-tabs--underline gds-nav-tabs--justified" data-gds-nav="7">
-            <li class="gds-nav-tabs__list-item gds-nav-tabs__list-item--underline gds-nav-tabs__list-item--active" data-gds-nav-item=""><a class="gds-nav-tabs__link gds-nav-tabs__link--underline" data-gds-nav-button="1" data-gds-nav-type="tab" href="/"><LogoButton />My Best Friend</a></li>
+        <nav className="-m-b-3" data-gds-nav-controls="">
+          <ul className="gds-nav-tabs gds-nav-tabs--underline gds-nav-tabs--justified" data-gds-nav="7">
+            <li className="gds-nav-tabs__list-item gds-nav-tabs__list-item--underline gds-nav-tabs__list-item--active" data-gds-nav-item=""><a className="gds-nav-tabs__link gds-nav-tabs__link--underline" data-gds-nav-button="1" data-gds-nav-type="tab" href="/"><LogoButton />My Best Friend</a></li>
 
-            <li class="gds-nav-tabs__list-item gds-nav-tabs__list-item--underline" data-gds-nav-item=""><a class="gds-nav-tabs__link gds-nav-tabs__link--underline" data-gds-nav-button="2" data-gds-nav-type="tab" href="/all_dogs">All Breeds</a></li>
+            <li className="gds-nav-tabs__list-item gds-nav-tabs__list-item--underline" data-gds-nav-item=""><a className="gds-nav-tabs__link gds-nav-tabs__link--underline" data-gds-nav-button="2" data-gds-nav-type="tab" href="/all_dogs">All Breeds</a></li>
           </ul>
         </nav>
       </header>
