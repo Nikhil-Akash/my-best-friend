60103a2ce7003d098e9150beccf6350b05434c9c
github username <github email address>
2018-06-14 21:57:03 -0700

    changed layout by removing __ensign from grid

--- src/container/SubBreedImgList.js
+++ src/container/SubBreedImgList.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
 class SubBreedImgList extends Component {
-
   render() {
+    const subBreedName = this.props.subBreedName
     const subBreedImg = this.props.subBreed.map((subBreedImg, index) =>
-      <div key={index} className="gds-ranker-grid__ensign">
-        <img key={index} className="gds-ranker-ensign__thumbnail gds-ranker-ensign__thumbnail--lg" src={subBreedImg} alt="Dog"/>
-        <span className="gds-ranker-ensign__number">
-          {index+1}
-        </span>
+      <div key={index} className="gds-ranker-grid">
+        <img key={index} className="gds-ranker-ensign__thumbnail gds-ranker-ensign__thumbnail--lg sub-breed-images" src={subBreedImg} alt="Dog"/>
+        <span className="gds-ranker-ensign__number">{index+1}</span>
       </div>);
 
     return (
