4c67002787e820b01b0b1f92e3b9eceb5580c1ed
github username <github email address>
2018-06-14 22:01:50 -0700

    removed ensign from grid

--- src/container/MainBreedImgList.js
+++ src/container/MainBreedImgList.js
@@ -4,7 +4,7 @@ class MainBreedImgList extends Component {
 
   render() {
     const allBreedImg = this.props.breed.map((image, index) =>
-      <div key={index} className="gds-ranker-grid__ensign">
+      <div key={index} className="gds-ranker-grid">
         <img key={index} className="gds-ranker-ensign__thumbnail gds-ranker-ensign__thumbnail--lg" src={image} alt="Dog"/>
         <span className="gds-ranker-ensign__number">
           {index+1}
