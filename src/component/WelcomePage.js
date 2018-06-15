e3c61e0f8bad8a59f0b3f8f86c4156f5c1cbf9f9
github username <github email address>
2018-06-14 20:05:47 -0700

    added dog image grid from freepik

--- src/component/WelcomePage.js
+++ src/component/WelcomePage.js
@@ -5,8 +5,10 @@ const WelcomePage = () => {
   return (
     <div className="-text-center">
       <h1 className="gds-text--header-lg">Welcome to My Best Friend</h1>
-      <p className=" -text-center ">put picture of a dog here</p>
-      <Link to={`/all_dogs`}><button type="button" id="allDogsButton" className="gds-button--default">All Dogs</button></Link>
+      <div>
+        <img src="../images/dogs.jpg" alt="Grid of Dogs by FreePik" height="400" width="400"/>
+      </div>
+      <Link to={`/all_dogs`}><button type="button" id="allDogsButton" className="gds-button--lg gds-button--success">See All Dogs</button></Link>
     </div>
   );
 }
